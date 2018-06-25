6e66e05c0b36a3e3c0e4564e64f43d48e07e5196
Joshua Jensch <jenschjoshua@gmail.com>
2018-06-25 02:16:14 +0200

    Migrate to MaybeT and catch exceptions at peer level

--- src/Peer.hs
+++ src/Peer.hs
@@ -13,6 +13,11 @@ module Peer where
   import Network.Socket hiding (recv, send, KeepAlive)
   import Network.Socket.ByteString (recv, send)
 
+  import Control.Monad.Trans.Maybe
+  import Control.Applicative (empty)
+
+  type MaybeIO = MaybeT IO
+
   type PartIdx = Word32
   type Begin = Word32
   type Length = Word32
@@ -36,41 +41,37 @@ module Peer where
 
   abcPeerId = take 20 ['A'..'Z']
 
-  connectToPeer :: String -> Word16 -> IO (Maybe Socket)
+  connectToPeer :: String -> Word16 -> MaybeIO Socket
   connectToPeer host port = do
-    addrInfo <- head <$> getAddrInfo Nothing (Just host) (Just $ show port)
-    sock <- socket (addrFamily addrInfo) Stream defaultProtocol
-    cr <- try $ timeout 1000000 (connect sock (addrAddress addrInfo))
+    addrInfo <- liftIO $ head <$> getAddrInfo Nothing (Just host) (Just $ show port)
+    sock <- liftIO $ socket (addrFamily addrInfo) Stream defaultProtocol
+    cr <- liftIO $ timeout 1000000 (connect sock (addrAddress addrInfo))
     case cr of
-      (Right (Just _)) -> do
-        putStrLn$"[=>] connected to "++(show.addrAddress) addrInfo
-        return $ Just sock
-      (Left (SomeException e)) -> return Nothing
-      _ -> return Nothing
+      Just _ -> do
+        liftIO $ putStrLn$"[=>] connected to "++(show.addrAddress) addrInfo
+        return sock
+      Nothing -> empty
 
-  doHandshake :: String -> Maybe Socket -> IO (Maybe Socket)
-  doHandshake _ Nothing = return Nothing
-  doHandshake infoHash (Just socket) = do
+  doHandshake :: String -> Socket -> MaybeIO Socket
+  doHandshake infoHash socket = do
     sent <- liftIO $ send socket (BS.pack $ handshake infoHash abcPeerId)
-    liftIO $ putStrLn $"[=>] send handshake to: " ++ show socket
     msgHandshake <- liftIO $ recv socket (8 + 20 + 40)
 
     let msgStatic = (BS.unpack.B.take (length handshakePrefix)) msgHandshake
-    if msgStatic /= handshakePrefix then
-      liftIO $ do
-        putStrLn $"[!] Received invalid handshake, abandoning peer. " ++ show (BS.pack msgStatic)
-        close socket
-        return Nothing
+    if msgStatic /= handshakePrefix then do
+      liftIO $ close socket
+      empty
     else do
       let (remoteInfoHash, remotePeerId) = (B.splitAt 20.(B.drop 28)) msgHandshake
       let infoHashMatches = BS.unpack remoteInfoHash == infoHash
-      liftIO $ putStrLn $"[<=] remote info: " ++ show remoteInfoHash
-      liftIO $ putStrLn $"[<=] remote peer: " ++ show remotePeerId
-      liftIO $ putStrLn $"[<=] infoHash is OK? " ++ show infoHashMatches
-      if infoHashMatches then return $ Just socket else return Nothing
+      liftIO $ putStrLn $"[<=] remote peerId: " ++ show remotePeerId
+      liftIO $ putStrLn $"[<=] remote advertised correct infoHash? " ++ show infoHashMatches
+      if infoHashMatches then return socket else empty
 
   testItWith :: String -> [(String, Word16)] -> IO ()
   testItWith infoHash peers = void $ parallel $
-    runSession <$> peers
+    (`catch` discardE).void.runMaybeT.runSession <$> peers
       where
         runSession (ip, port) = connectToPeer ip port >>= doHandshake infoHash
+  discardE :: SomeException -> IO ()
+  discardE = print
